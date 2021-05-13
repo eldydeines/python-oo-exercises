5290581cfebf252dfe92d07381c5900be5d787f1
eldydeines <eldy.deines@outlook.com>
2021-05-13 16:49:14 -0600

    finished exercises

--- serial.py
+++ serial.py
@@ -1,8 +1,11 @@
-"""Python serial number generator."""
+""" Python serial number generator.
+    Eldy Deines - May 13 --
+"""
+
 
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -20,3 +23,20 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        """Creates unique number for instance"""
+        self.start = start - 1
+        self.serial = self.start
+
+    def __repr__(self):
+        """Represent the class"""
+        return f"<SerialGenerator start={self.start} next={self.serial+1}>"
+
+    def generate(self):
+        """Increments serial number"""
+        self.serial += 1
+        return self.serial
+
+    def reset(self):
+        """Resets back to the original start number provided"""
+        self.serial = self.start
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,49 @@
 """Word Finder: finds random words from a dictionary."""
+from random import choice
 
 
 class WordFinder:
-    ...
+    """Gets list of words from file and can randomly select."""
+
+    def __init__(self, file_name):
+        """Creates instance of wordfinder"""
+        self.file_name = file_name
+
+        def get_words(self):
+            input_file = open(self.file_name, 'r')
+            words = [line.strip('\n') for line in input_file]
+            input_file.close()
+            return words
+
+        self.lst_of_words = get_words(self)
+
+    def __repr__(self):
+        """Making a representation of the class"""
+        count = len(self.lst_of_words)
+        return f"{count} words read"
+
+    def random(self):
+        """Conducts a random pull from the list of words stored"""
+        return choice(self.lst_of_words)
+
+
+class RandomWordFinder(WordFinder):
+    """Just in case file has weird stuff in it like # or blank lines
+
+        >>> rwf = RandomWordFinder("crazy_words.txt")
+        6 words read
+
+    """
+
+    def __init__(self, file_name):
+
+        super().__init__(file_name)
+
+        def get_words(self):
+            input_file = open(self.file_name, 'r')
+            words = [line.strip('\n') for line in input_file if line != "\n" and not(
+                line.startswith('#'))]
+            input_file.close()
+            return words
+
+        self.lst_of_words = get_words(self)
